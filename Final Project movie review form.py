9a875326b1e20fed1aeb6487634bbcec812dde4e
Crusher2503 <101029048+Crusher2503@users.noreply.github.com>
2022-03-05 21:36:55 -0500

    Add files via upload

---
+++ Final Project movie review form.py
@@ -0,0 +1,5 @@
+"""  Movie review form final project
+This code allows you to input a movie title your rating for said movie and
+will give the option to leave more reivews or to quit out of the app.
+Tim Powers 3/1/2022
+"""
